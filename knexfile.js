a11ff72235a0e2c769700f47b618dfb40a32e051
Donald Whitely <dswhitely1@gmail.com>
2019-10-22 14:05:30 -0400

    Defined KnexConfig

---
+++ knexfile.js
@@ -0,0 +1,71 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  testing: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_TEST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
